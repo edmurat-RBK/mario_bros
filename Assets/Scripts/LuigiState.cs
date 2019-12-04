9d5f0a3b5098aec8c2d3de47bea43565ab6f1892
Edouard Murat <e.murat@rubika-edu.com>
2019-12-05 00:51:47 +0100

    Added LuigiState enum

---
+++ Assets/Scripts/LuigiState.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LuigiState
+{
+    AT_FLOOR_1_ARMS_DOWN,
+    AT_FLOOR_1_ARMS_UP,
+    AT_FLOOR_2_ARMS_DOWN,
+    AT_FLOOR_2_ARMS_UP,
+    AT_FLOOR_3,
+    DROPPING,
+    TAKING_BREAK,
+    GETTING_YELLED,
+    GETTING_YELLED_BOWING,
+    DRIVING
+}
