d6fd96df1a7b059d88ff09466fc01c19056353ac
Edouard Murat <e.murat@rubika-edu.com>
2019-12-05 00:52:01 +0100

    Created LuigiManager

---
+++ Assets/Scripts/LuigiManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EDM_Luigi
+{
+    public class LuigiManager : MonoBehaviour
+    {
+
+        public LuigiState state;
+
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
