63d3f56fc6f1482c573e45bca6880e9bcab54f35
Edouard MURAT <e.murat@rubika-edu.com>
2019-12-05 16:47:27 +0100

    Added Mario states, aprite and first controls

---
+++ Assets/Scripts/Mario/MarioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EDM_Mario
+{
+    public class MarioManager : MonoBehaviour
+    {
+
+        public MarioState state;
+
+
+        void Start()
+        {
+            state = MarioState.RECEIVING;
+        }
+
+        void Update()
+        {
+            switch (state)
+            {
+                case MarioState.RECEIVING:
+                    if (Input.GetKeyDown(KeyCode.UpArrow))
+                    {
+                        state = MarioState.AT_FLOOR_2_ARMS_DOWN;
+                    }
+                    break;
+
+                case MarioState.AT_FLOOR_2_ARMS_DOWN:
+                    if (Input.GetKeyDown(KeyCode.UpArrow))
+                    {
+                        state = MarioState.AT_FLOOR_3_ARMS_DOWN;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.DownArrow))
+                    {
+                        state = MarioState.RECEIVING;
+                    }
+                    break;
+
+                case MarioState.AT_FLOOR_3_ARMS_DOWN:
+                    if (Input.GetKeyDown(KeyCode.DownArrow))
+                    {
+                        state = MarioState.AT_FLOOR_2_ARMS_DOWN;
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Mario/MarioState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MarioState
+{
+    AT_FLOOR_1,
+    AT_FLOOR_2_ARMS_DOWN,
+    AT_FLOOR_2_ARMS_UP,
+    AT_FLOOR_3_ARMS_DOWN,
+    AT_FLOOR_3_ARMS_UP,
+    RECEIVING,
+    TAKING_BREAK,
+    GETTING_YELLED,
+    GETTING_YELLED_BOWING
+}
