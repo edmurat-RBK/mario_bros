0a153ee0439dc4ed7d5a16007af87d3d1e99154e
Edouard MURAT <e.murat@rubika-edu.com>
2019-12-05 16:55:55 +0100

    Created GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EDM_Luigi;
+using EDM_Mario;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private LuigiManager luigiManager;
+    private MarioManager marioManager;
+
+    [Header("Game loop")]
+    public float tickSpeed;
+    public bool loopActive = true;
+
+    [Header("Boxes")]
+    public float boxSpawnChance;
+
+    private void Start()
     {
-        
+        luigiManager = GameObject.FindGameObjectWithTag("LuigiManager").GetComponent<LuigiManager>();
+        marioManager = GameObject.FindGameObjectWithTag("MarioManager").GetComponent<MarioManager>();
+        StartCoroutine(Tick());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Tick()
     {
-        
+        while(loopActive)
+        {
+            // What I need to do in the loop ?
+            Debug.Log("New Tick : " + Time.time);
+            yield return new WaitForSeconds(tickSpeed);
+        }
     }
 }
